f219557aa24809a65833cb44f53a85bf65715ba4
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 09:53:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+array=holiday_hash[;winter].map
 end
 
 def all_supplies_in_holidays(holiday_hash)
