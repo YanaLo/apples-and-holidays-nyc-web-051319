37af338766897993dc6df5fb28bd7e941a7fb0d8
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 09:54:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,8 @@ array=holiday_hash[:winter].map
 do |holiday,i|
 i
 end
-
+array.flatten
+end
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
