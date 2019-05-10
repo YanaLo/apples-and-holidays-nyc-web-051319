0c5bbc082874fd723c78a5108a5f7fd41af8a0fd
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 10:03:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ puts "#{season.capitalize}:"
        puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
   end 
-end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
